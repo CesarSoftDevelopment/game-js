eb89a3671708f8b602ce62a14497c64a0ea50ba7
= <=>
2021-09-27 22:26:00 -0300

    added a function to create a component function to add a component to the screen

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ bodyPage = document.querySelector("body");
 // function that plays an area of ​​the game
 bodyPage.onload = function startGame(){
     myGameArea.start();
+    //instantiating the constructor function
+    myGamePiece = new component(30, 30, 'blue', 50, 50);
 }
 
 // object to create game area
@@ -17,4 +19,17 @@ const myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
+}
+
+// function to create the component
+function component(width, height, color, x, y){
+    this.width = width;
+    this.height = height;
+    this.color = color;
+    this.x = x;
+    this.y = y;
+    ctx = myGameArea.context;
+    ctx.fillStytle = color;
+    ctx.fillRect = (this.x, this.y, this.width, this.height);
+
 }
